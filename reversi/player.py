ddbdcef33c8fd8d3395b0d184268b08d2010baed
Ales Shvaibovich <neironis94@gmail.com>
2022-11-13 19:07:05 +0100

    final version

--- reversi/player.py
+++ reversi/player.py
@@ -11,22 +11,22 @@ from copy import deepcopy
 '''----------Constants for MyPlayer----------'''
 BLANK = -1  #-1 represents blank field
 BOARD_SIZE = 8
-ALL_DIRECTIONS = [(1,1), (1,0), (1,-1), (0,-1), (-1,-1), (-1,0), (-1,1), (0,1)] 
-#Matrix FIELDS_VALUES represents value of each field
-FIELDS_VALUES = [[ 25, -10,  5,  5,  5,  5, -10,  25],
-                 [-10, -10, -2, -2, -2, -2, -10, -10],
-                 [  5,  -2,  0,  0,  0,  0,  -2,   5],
-                 [  5,  -2,  0,  0,  0,  0,  -2,   5],
-                 [  5,  -2,  0,  0,  0,  0,  -2,   5],
-                 [  5,  -2,  0,  0,  0,  0,  -2,   5],
-                 [-10, -10, -2, -2, -2, -2, -10, -10],
-                 [ 25, -10,  5,  5,  5,  5, -10,  25]]
+ALL_DIRECTIONS = [(1,1), (1,0), (1,-1), (0,-1), (-1,-1), (-1,0), (-1,1), (0,1)]
 #Initial values for minimax strategy with alpha-beta pruning
 START_ALPHA = -inf
 START_BETA = inf
 START_DEPTH = 3 #3 is maximum possible depth of algorithm with time limit 1 sec
 START_MAXIMIZING_PLAYER = True  #True - my turn, False - opponent's turn
- 
+#Matrix FIELDS_VALUES represents value of each field
+FIELDS_VALUES = [[20, -5,  5,  5,  5,  5, -5, 20],
+                 [-5, -5, -2, -2, -2, -2, -5, -5],
+                 [ 5, -2,  0,  0,  0,  0, -2,  5],
+                 [ 5, -2,  0,  0,  0,  0, -2,  5],
+                 [ 5, -2,  0,  0,  0,  0, -2,  5],
+                 [ 5, -2,  0,  0,  0,  0, -2,  5],
+                 [-5, -5, -2, -2, -2, -2, -5, -5],
+                 [20, -5,  5,  5,  5,  5, -5, 20]]
+
 
 class MyPlayer:
     '''Player uses minimax strategy with alpha-beta pruning with depth = 3'''
@@ -34,7 +34,7 @@ class MyPlayer:
         self.name = 'shvaiale'
         self.my_color = my_color
         self.opp_color = opponent_color
-  
+
  
     def move(self, board):
         #At first I check, if at least one possible move exists
@@ -47,8 +47,8 @@ class MyPlayer:
         self.minimax(board, START_MAXIMIZING_PLAYER, \
             START_DEPTH, START_ALPHA, START_BETA)
         return self.optimal_move
-        
- 
+
+
     '''----------Functions for finding possible moves----------'''
     @staticmethod
     def find_moves(board, my_color, opp_color):
@@ -97,6 +97,7 @@ class MyPlayer:
                 nearby_blanks.add((cur_i, cur_j))
         return nearby_blanks
 
+
     @staticmethod
     def is_sequence(board, i, j, dir_i, dir_j, my_color, opp_color):
         #Look from field (i, j) in direction (dir_i, dir_j) and
@@ -129,56 +130,87 @@ class MyPlayer:
                 return True
         #return False, if we've gone off a loop (= gone off a board)
         return False
- 
+
+
     @staticmethod
     def is_on_board(a, b):
         #Check, if (a, b) is on the board
         return True if 0 <= a < BOARD_SIZE and 0 <= b < BOARD_SIZE else False
 
-    '''----------Strategy functions----------'''
 
+    '''----------Strategy functions----------'''
     def minimax(self, board, maximizingPlayer, depth, alpha, beta):
-        if self.game_ended(board):
+        #Classic minimax with alpha-beta pruning. This recursive algorithm
+        #looks three moves ahead and chooses the move, that will lead
+        #to the best position.
+        #maximizingPlayer = True means it's my turn to choose
+        #evaluation now and I will choose maximal,
+        #maximizingPlayer = False means it's opponent's turn
+        #to choose now and he will choose minimal.
+        #You can watch more about this strategy in "Used materials"
+
+        #if I won, then I obviosly want to reach this position (return inf),
+        #else I lost or it's a draw (return -inf)
+        if self.game_finished(board):
             if self.I_won(board):
                 return inf
             else:
                 return -inf
-
+        #if maximal depth of recursion is reached, 
+        #then return value of the position 
         if depth == 0:
             return self.count_position(board)
-
+        #if it's my turn now, then find the maximal value of a position,
+        #that can be reached in one move, and return it
         if maximizingPlayer:
-            max_eval = -inf
+            max_eval = -inf #initial maximal evaluation
+            #find my possible moves
             moves = MyPlayer.find_moves(board, self.my_color, self.opp_color)
+            #if there is no any moves, then I skip the turn
             if not moves:
-                return self.minimax(board, True, depth - 1, alpha, beta)
+                return self.minimax(board, False, depth - 1, alpha, beta)
+            #look throw all moves and find the best position
             for move in moves:
-                new_board = MyPlayer.update_board(board, move, self.my_color)
+                #compute board, which will be reached with the particular move
+                new_board = self.update_board(board, move, self.my_color)
+                #compute the best evaluation for the particular move
                 eval = self.minimax(new_board, False, depth - 1, alpha, beta)
                 if eval >= max_eval:
                     max_eval = eval
+                    #if it's the upper level of recursion, then
+                    #set self.optimal_move on the current move
                     if depth == START_DEPTH:
                         self.optimal_move = move
+                #alpha-beta pruning
                 alpha = max(alpha, eval)
                 if beta <= alpha:
                     break
             return max_eval
+        #else it's opponent's turn now. Find the minimal value of a position,
+        #that can be reached in one move, and return it
         else:
-            min_eval = inf 
+            min_eval = inf  #initial minimal evaluation
+            #find opponent's possible moves
             moves = MyPlayer.find_moves(board, self.opp_color, self.my_color)
+            #if there is no any moves, then he skips the turn
             if not moves:
-                return self.minimax(board, False, depth - 1, alpha, beta)
+                return self.minimax(board, True, depth - 1, alpha, beta)
+            #look throw all moves and find the worst position
             for move in moves:
-                new_board = MyPlayer.update_board(board, move, self.opp_color)
+                #compute board, which will be reached with the particular move
+                new_board = self.update_board(board, move, self.opp_color)
+                #compute the worst evaluation for the particular move
                 eval = self.minimax(new_board, True, depth - 1, alpha, beta)
                 min_eval = min(min_eval, eval)
+                #alpha-beta pruning
                 beta = min(beta, eval)
                 if beta <= alpha:
                     break
             return min_eval
 
 
-    def game_ended(self, board):
+    def game_finished(self, board):
+        #Game is finished, if neither I, nor opponent has any moves
         my_moves = MyPlayer.find_moves(board, self.my_color, self.opp_color)
         if my_moves:
             return False
@@ -189,7 +221,12 @@ class MyPlayer:
 
         return True
 
+
     def I_won(self, board):
+        #I won, if game is finished and I have more stones, than the opponent 
+        if not self.game_finished(board):
+            return False
+
         my_stones = 0
         opp_stones = 0
         for line in board:
@@ -202,55 +239,55 @@ class MyPlayer:
 
 
     def count_position(self, board):
-        result = 0
+        #Function counts evaluation of the particular position.
+        #Evaluation = MS + MFV - OS - OFV, where
+        #MS - number of my stones, OS - number of opponent's stones,
+        #MFV - sum of values of fields, where my stones are placed
+        #OFV - sum of values of fields, where opponent's stones are placed
+        eval = 0
         for i in range(BOARD_SIZE):
             for j in range(BOARD_SIZE):
                 stone = board[i][j]
                 if stone == self.my_color:
-                    result += 1
-                    result += FIELDS_VALUES[i][j]
+                    eval += 1
+                    eval += FIELDS_VALUES[i][j]
                 elif stone == self.opp_color:
-                    result -= 1
-                    result -= FIELDS_VALUES[i][j]
-        return result
+                    eval -= 1
+                    eval -= FIELDS_VALUES[i][j]
+        return eval
 
-    @staticmethod
-    def update_board(board, move, color):
-        another_color = 0
-        if color == 0:
-            another_color = 1
+
+    def update_board(self, board, move, color):
+        #Function returns board after adding stone (color of stone = color)
+        #placed on the field (i, j) = (move[0], move[1])
         
-        new_board = deepcopy(board)
         i, j = move[0], move[1]
+        #set second color
+        sec_color = self.opp_color
+        if color == self.opp_color:
+            sec_color = self.my_color
+        #create new board
+        new_board = deepcopy(board)
         new_board[i][j] = color
+        #flip all stones (with second color) in all
+        #directions, if they are in a sequence
         for dir_i, dir_j in ALL_DIRECTIONS:
-            if MyPlayer.is_sequence(new_board, i, j, dir_i, dir_j, color, another_color):
+            if MyPlayer.is_sequence(new_board, i, j, dir_i, dir_j, color, sec_color):
                 MyPlayer.flip_stones(new_board, i, j, dir_i, dir_j, color)
         return new_board
 
 
     @staticmethod
     def flip_stones(board, i, j, dir_i, dir_j, color):
+        #Function assumes, that there is a sequence to be fliped
+        #from field (i, j) in the direction (dir_i, dir_j). Function
+        #changes all stones in this direction to stone with 'color'
+
+        #current stone to be fliped (cur_i, cur_j)
         cur_i = i + dir_i
         cur_j = j + dir_j
-
+        #flip stones until stone with 'color' is encountered 
         while board[cur_i][cur_j] != color:
             board[cur_i][cur_j] = color
             cur_i += dir_i
             cur_j += dir_j
-    
-
-
-
-if __name__ == "__main__":
-    player = MyPlayer(0, 1)
-    board = [[-1, -1, -1, -1, -1,  1, -1,  0],
-             [-1, -1, -1,  1, -1, -1,  1, -1],
-             [-1, -1, -1, -1,  1,  0,  1,  1],
-             [-1, -1, -1,  0,  0,  1, -1, -1],
-             [-1, -1,  0,  0,  0,  1, -1, -1],
-             [-1, -1,  0,  0,  0, -1, -1, -1],
-             [-1, -1,  0, -1,  0, -1, -1, -1],
-             [-1, -1, -1, -1, -1, -1, -1, -1]]
-     
-    print(player.move(board))
